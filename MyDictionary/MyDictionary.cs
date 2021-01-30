4cdc04e8dfa6a426066f0854cd22f10cc1917b96
Enes Musaoğlu <emusaoglu00@outlook.com>
2021-01-31 02:05:06 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] keyArray;
+        V[] valueArray;
+
+        public MyDictionary()
+        {
+            keyArray = new K[0];
+            valueArray = new V[0];
+        }
+        public void Add(K key,V value)
+        {
+            K[] tempKey = keyArray;
+            V[] tempValue = valueArray;
+            keyArray = new K[keyArray.Length + 1];
+            valueArray = new V[valueArray.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keyArray[i] = tempKey[i];
+            }
+            keyArray[keyArray.Length - 1] = key;
+            for (int j = 0; j < tempValue.Length; j++)
+            {
+                valueArray[j] = tempValue[j];
+            }
+            valueArray[valueArray.Length - 1] = value;
+
+            Console.WriteLine("Id : "+key + "Name : " + value);
+        }
+
+    }
+}
+
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(10,"Lionel Messi");
+            dictionary.Add(7,"Cristiano Ronaldo");
+            dictionary.Add(11,"Mesut Özil");
+            dictionary.Add(1,"Manuel Neuer");
+           
+        }
+    }
+}
